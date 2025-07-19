8713e50016b1a63eb39663320a27aa0149371b9b
Akhil Nishad <akhilnishadofficial@gmail.com>
2025-07-19 19:40:45 +0530

    looking good

--- components/sections/ContactCTASection.tsx
+++ components/sections/ContactCTASection.tsx
@@ -36,7 +36,7 @@ export default function ContactCTASection({
         opacity: 1,
         y: 0,
         duration: 0.8,
-        ease: "power2.out",
+        ease: "power4.inOut",
         stagger: 0.2,
       },
     );
